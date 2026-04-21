17beb0ac849146b9027495bf5c9941f5d5048277
Saejima-Kouga <847436609@qq.com>
2026-04-21 17:37:24 +0800

    User实体类
    普通用户实体类和属性（userId，userName，password，email，phoneNum）

---
+++ src/main/java/com/lhz/SETSS2026/entity/User.java
@@ -0,0 +1,20 @@
+package com.lhz.SETSS2026.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private Long userId;
+    private String userName;
+    private String passWord;
+    private String email;
+    private Integer phoneNum;
+}
