fef190d7068436df4e132490962bb13e9c735da7
huaziqi <2946048113@qq.com>
2026-04-21 16:25:03 +0800

    init

---
+++ src/main/java/com/lhz/SETSS2026/Setss2026Application.java
@@ -0,0 +1,13 @@
+package com.LHZ.SETSS2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Setss2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Setss2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/lhz/SETSS2026/Setss2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LHZ.SETSS2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Setss2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
