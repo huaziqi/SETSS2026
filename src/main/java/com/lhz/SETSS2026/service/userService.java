a7bd63609930e4690e7626f3c30092345462d01c
GA2025 <3364146884@qq.com>
2026-04-22 21:13:14 +0800

    Update userService.java

--- src/main/java/com/lhz/SETSS2026/service/userService.java
+++ src/main/java/com/lhz/SETSS2026/service/userService.java
@@ -1,5 +1,17 @@
 package com.lhz.SETSS2026.service;
 
-public class UserService {
+import com.lhz.SETSS2026.dto.AuthResponse;
+import com.lhz.SETSS2026.entity.User;
 
+public class UserService {
+    public String register(User user) {
+        return null;
+    }
+
+    public AuthResponse login(String username, String password) {
+        if (username.equals("admin") && password.equals("123")){
+            return new AuthResponse("0");
+        }
+        return new AuthResponse("1");
+    }
 }
